a06ddcc6704b492fb508ca54b6b795e0669c7ebb
Celalettin Elbir <celallelbir@hotmail.com>
2023-07-22 14:49:07 +0300

    last commit

--- main/main/settings.py
+++ main/main/settings.py
@@ -21,11 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-jt!p!i6bge!7i!t89vq)zo(a@9pm04)*xx&=-vq*l^qldwp0%l'
-
+SECRET_KEY =os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("IS_DEVELOPMENT",True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv("APP_HOST")
+]
 
 
 # Application definition
@@ -141,6 +143,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
