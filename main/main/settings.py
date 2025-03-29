6d3acd1fde4aa91a089749f1a74a69d37eea9edf
Celalettin Elbir <celallelbir@hotmail.com>
2025-03-29 10:11:22 +0300

    fix: update settings.py to enable environment-based configuration for DEBUG and HTTPS settings; specify allowed hosts for production

--- main/main/settings.py
+++ main/main/settings.py
@@ -27,20 +27,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-jt!p!i6bge!7i!t89vq)zo(a@9pm04)*xx&=-vq*l^qldwp0%l')
 
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 # ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'miarsmimarlik.com www.miarsmimarlik.com').split()
 
-# # HTTPS settings
-# SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT', 'True') == 'True'
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE', 'True') == 'True'
-# CSRF_COOKIE_SECURE = os.environ.get('CSRF_COOKIE_SECURE', 'True') == 'True'
-# SECURE_HSTS_SECONDS = 31536000  # 1 year
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
-ALLOWED_HOSTS = ['*']
+# HTTPS settings
+SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT', 'True') == 'True'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE', 'True') == 'True'
+CSRF_COOKIE_SECURE = os.environ.get('CSRF_COOKIE_SECURE', 'True') == 'True'
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+ALLOWED_HOSTS = ['miarsmimarlik.com', 'www.miarsmimarlik.com']
 
 # Application definition
 
